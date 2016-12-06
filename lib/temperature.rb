792c01bf09108cc0b498d489747f3544a1400600
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2016-12-06 16:04:00 +0100

    test to_kelvin output nil

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -5,6 +5,7 @@ class Temperature
     FAHRENHEIT_OFFSET = 32
     FAHRENHEIT_SLOPE = Rational(9,5)
 
+
     def initialize temperature
         throw Exception unless temperature.kind_of? Numeric
         @temperature = temperature
@@ -12,6 +13,10 @@ class Temperature
 
     def to_fahrenheit
         FAHRENHEIT_OFFSET + temperature * FAHRENHEIT_SLOPE
+    end
+
+    def to_kelvin
+      
     end
 
 end
